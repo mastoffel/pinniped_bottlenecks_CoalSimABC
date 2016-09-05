fb6d21486a4a4adee85d9047f1617dd0756e4ebd
martin <martin.adam.stoffel@gmail.com>
2016-09-05 10:38:15 +0200

    simulation scripts updated

--- analyse_sim.R
+++ analyse_sim.R
@@ -3,32 +3,47 @@ library(devtools)
 # install_github("mastoffel/sealABC")
 library(sealABC)
 library(data.table)
+library(reshape2)
+library("abctools")
+library(abc)
+library(ggplot2)
 
-sims <- fread("sims_50000.txt", stringsAsFactors = FALSE)
+sims <- fread("sims_50000_6mod.txt", stringsAsFactors = FALSE)
+# sims <- as.data.frame(sims)
+
+sims2 <- fread("sims_50000_6mod_nr2.txt", stringsAsFactors = FALSE)
+# sims2 <- as.data.frame(sims2)
+
+sims3 <- fread("sims_50000_6mod_nr3.txt", stringsAsFactors = FALSE)
+# sims3 <- as.data.frame(sims2)
+
+sims <- rbindlist(list(sims, sims2, sims3), use.names = TRUE, fill = TRUE)
+
 sims <- as.data.frame(sims)
-# sims <- read.table("sims_full_broad_priors.txt", stringsAsFactors = FALSE)
-# names(sims) <- unlist(sims[1, ])
-# sims <- sims[-1, ]
-# transform everything to numeric
-# sims <- cbind(do.call(cbind, lapply(sims[-ncol(sims)], as.numeric)), sims[ncol(sims)])
 
-#
+# sims_melt <- data.table::melt(sims, id.vars )
+
+sims_melt <- melt(sims[c(15,17,28)], na.rm = TRUE, id.var = "model")
+ggplot(data = sims_melt, aes(x = variable, y = value)) + 
+        geom_boxplot(aes(x = model)) +
+        facet_wrap(~ variable)
+        
+
 par(mfcol = c(4,4))
 
 boxplot(sims$num_alleles_mean ~ sims$model)
-boxplot(sims$mean_allele_size_sd ~ sims$model)
+boxplot(sims$mean_allele_size_var ~ sims$model)
 boxplot(sims$mean_allele_range ~ sims$model)
 boxplot(sims$exp_het_mean ~ sims$model)
 boxplot(sims$obs_het_mean ~ sims$model)
 boxplot(sims$mratio_mean ~ sims$model)
-boxplot(sims$g2 ~ sims$model)
+# boxplot(sims$g2 ~ sims$model)
 boxplot(sims$het_excess ~ sims$model)
 
-library(ggplot2)
 
 par(mfcol = c(3,3))
 boxplot(sims$num_alleles_sd ~ sims$model)
-boxplot(sims$sd_allele_size_sd ~ sims$model)
+boxplot(sims$sd_allele_size_var ~ sims$model)
 boxplot(sims$sd_allele_range ~ sims$model)
 boxplot(sims$exp_het_sd ~ sims$model)
 boxplot(sims$obs_het_sd ~ sims$model)
@@ -41,26 +56,30 @@ all_seals <- sealABC::read_excel_sheets("../data/seal_data_largest_clust_and_pop
 lapply(all_seals, function(x) range(unlist(x[4:ncol(x)]), na.rm = TRUE))
 
 # hawaiian monk seal
-genotypes <- all_seals[[36]]
+genotypes <- all_seals[[13]]
 genotypes <- genotypes[4:ncol(genotypes)]
 
 # calculate summary statistics
 obs_stats <- mssumstats(genotypes, type = "microsats")
 
 # divide stats and parameters
-sims_stats <- sims[15:(ncol(sims)-1)] # last column is model factor
-sims_param <- sims[1:14]
+sims_stats <- sims[19:(ncol(sims)-1)] # last column is model factor
+sims_param <- sims[1:18]
 models <- sims[["model"]]
 
-library(abc)
+?AS.select
+# ASchoice <- AS.select(obs_stats, sims_param, sims_stats)
+# ASchoice$best
 
 # select best summary statistics
-sims_stats <- sims_stats[c("num_alleles_mean", "mean_allele_range", "mean_allele_size_sd", "exp_het_mean")]
-obs_stats <- obs_stats[c("num_alleles_mean", "mean_allele_range", "mean_allele_size_sd", "exp_het_mean")]
+sims_stats <- sims_stats[c("num_alleles_mean", "mean_allele_range", "exp_het_mean", "mean_allele_size_var", "mratio_mean", "het_excess" )] # "mean_allele_size_var"
+obs_stats <- obs_stats[c("num_alleles_mean", "mean_allele_range", "exp_het_mean","mean_allele_size_var", "mratio_mean", "het_excess" )] # "mratio_mean",
 
 
 # can abc distinguish between the 4 models ?
-cv.modsel <- cv4postpr(models, sims_stats, nval=5, tol=.01, method="rejection")
+# sims_stats <- as.matrix(sims_stats)
+# sims_stats[is.infinite(sims_stats)] <- NA
+cv.modsel <- cv4postpr(models, sims_stats, nval=100, tol=.01, method="rejection", subset = !(is.infinite(sims_stats$mratio_mean)))
 s <- summary(cv.modsel)
 plot(cv.modsel, names.arg=c("bot", "neut", "decl", "exp"))
 
@@ -68,28 +87,29 @@ plot(cv.modsel, names.arg=c("bot", "neut", "decl", "exp"))
 # model probabilities
 mod_prob <- postpr(obs_stats, models, sims_stats, tol = 0.001, method = "mnlogistic")
 summary(mod_prob)
-mod_prob <- postpr(obs_stats, models, sims_stats, tol = 0.01, method = "rejection")
+mod_prob <- postpr(obs_stats, models, sims_stats, tol = 0.001, method = "rejection")
 summary(mod_prob)
-mod_prob <- postpr(obs_stats, models, sims_stats, tol = 0.001, method = "neuralnet")
+mod_prob <- postpr(obs_stats, models, sims_stats, tol = 0.05, method = "neuralnet")
 summary(mod_prob)
 
 
 # goodness of fit
 sims_stats <- as.matrix(sims_stats)
+# sims_stats <- sims_stats[-(rowSums(is.infinite(sims_stats))>0), ]
 
-res_gfit_bott <- gfit(target=obs_stats, sumstat=sims_stats[models == "bot",], tol = 0.001, statistic=median, nb.replicate=100)
+res_gfit_bott <- gfit(target=obs_stats, sumstat=sims_stats[models == "bot",], tol = 0.01, statistic=mean, nb.replicate=10)
 plot(res_gfit_bott, main="Histogram under H0")
 summary(res_gfit_bott)
 
-res_gfit_neut <- gfit(target=obs_stats, sumstat= sims_stats[models == "neut",], statistic=mean,tol = 0.01,  nb.replicate=100)
+res_gfit_neut <- gfit(target=obs_stats, sumstat= sims_stats[models == "neut",], statistic=mean,tol = 0.01,  nb.replicate=10)
 plot(res_gfit_neut, main="Histogram under H0")
 summary(res_gfit_neut)
 
-res_gfit_decl <- gfit(target=obs_stats, sumstat= sims_stats[models == "decl",], statistic=mean,tol = 0.01,  nb.replicate=100)
+res_gfit_decl <- gfit(target=obs_stats, sumstat= sims_stats[models == "decl_IA",], statistic=mean,tol = 0.01,  nb.replicate=10)
 plot(res_gfit_decl, main="Histogram under H0")
 summary(res_gfit_decl)
 
-res_gfit_exp <- gfit(target=obs_stats, sumstat= sims_stats[models == "exp",], statistic=mean, tol = 0.001, nb.replicate=100)
+res_gfit_exp <- gfit(target=obs_stats, sumstat= sims_stats[models == "exp_IA",], statistic=mean, tol = 0.01, nb.replicate=100)
 plot(res_gfit_exp, main="Histogram under H0")
 summary(res_gfit_exp)
 
@@ -98,22 +118,22 @@ sims_stats_new <- apply(sims_stats, 2, function(x){
   x[!is.finite(x)] <- mean(x[is.finite(x)], na.rm =TRUE)
   x
 } )
-gfitpca(target=obs_stats, sumstat=sims_stats_new, index=models, cprob=.01)
+gfitpca(target=obs_stats, sumstat=sims_stats_new, index=models, cprob=0.1)
 
 
 # posterior predictive checks (Gelman) // critics: using the data twice. 
-mylabels <- c("num_alleles_mean", "mean_allele_range", "mean_allele_size_sd", "exp_het_mean")
+mylabels <- c("num_alleles_mean", "mean_allele_range", "mean_allele_size_var", "exp_het_mean", "mratio_mean", "het_excess")
 # mylabels <- c("num_alleles_mean", "mean_allele_range", "mratio_mean")
 par(mfrow = c(2,3), mar=c(5,2,4,0))
 for (i in mylabels){
-  hist(sims_stats[models == "bot",i],breaks=40, xlab=i, main="")
+  hist(sims_stats[models == "exp_rec",i],breaks=40, xlab=i, main="")
   abline(v = obs_stats[i], col = 2)
 }
 
 
 
 # parameter inference under the specified model
-mod <- "exp"
+mod <- "exp_rec"
 
 stat_bot <- subset(sims_stats, subset=models==mod)
 head(stat_bot)
@@ -122,8 +142,8 @@ par_bot <- subset(sims_param, subset=models==mod)
 head(par_bot)
 
 # before inference, we see whether a parameter can be estimated at all
-cv_res_rej <- cv4abc(data.frame(Na=par_bot[,"start_exp"]), stat_bot, nval=100,
-                     tols=c(.001,.005, .01), method="loclinear", statistic = "mean")
+cv_res_rej <- cv4abc(data.frame(Na=par_bot[,"N_bot"]), stat_bot, nval=100,
+                     tols=c(.01), method="loclinear", statistic = "mean")
 summary(cv_res_rej) #should be as low as possible
 plot(cv_res_rej, caption = "rejection")
 
@@ -132,16 +152,18 @@ plot(cv_res_rej, caption = "rejection")
 library(dplyr)
 par_bot <- par_bot %>% 
   mutate(N_bot = N0 * N_bot) %>%
-  mutate(N_hist = N0 * N_hist) %>%
+  mutate(N_hist_bot = N0 * N_hist_bot) %>%
   mutate(N_hist_decl = N0 * N_hist_decl) %>%
   mutate(N_hist_exp = N0 * N_hist_exp) %>%
   mutate(start_bot = 4 * N0 * start_bot) %>%
   mutate(end_bot = 4 * N0 * end_bot)  %>%
-  mutate(start_decl = 4 * N0 * start_decl) %>%
-  mutate(start_exp = 4 * N0 * start_exp)
+  mutate(start_decl_IA = 4 * N0 * start_decl_IA) %>%
+  mutate(start_decl_recent = 4 * N0 * start_decl_recent) %>%
+  mutate(start_exp_IA = 4 * N0 * start_exp_IA) %>%
+  mutate(start_exp_recent = 4 * N0 * start_exp_recent)
 
-res <- abc(target = unlist(obs_stats), param = par_bot[, "start_exp"], 
-           sumstat = stat_bot, tol = 0.001, method="loclinear")
+res <- abc(target = unlist(obs_stats), param = par_bot[, "start_exp_recent"], 
+           sumstat = stat_bot, tol = 0.01, method="loclinear")
 
 summary(res)
 hist(res, breaks = 100)
--- sim_msats.R
+++ sim_msats.R
@@ -3,7 +3,7 @@
 library(devtools)
 # install_github("mastoffel/sealABC", force = TRUE)
 library(sealABC)
-# devtools::install_github("andersgs/microsimr")
+# devtools::install_github("andersgs/microsimr", build_vignettes = TRUE, force = TRUE)
 library(microsimr)
 library(strataG)
 library(splitstackshape)
@@ -12,7 +12,7 @@ library(parallel)
 library(data.table)
 library(readxl)
 library(dplyr)
-
+library(truncnorm)
 # devtools::install_github('ericarcher/strataG', dependencies = TRUE)
 
 # load all seal data ------------------------------
@@ -20,66 +20,70 @@ all_seals <- sealABC::read_excel_sheets("../data/seal_data_largest_clust_and_pop
 
 
 ### select 28 datasets //
-seals <- all_seals[c("antarctic_fur_seal", "galapagos_fur_seal", "stellers_sea_lion_cl_1",
-                     "grey_seal_orkneys", "harbour_seal_waddensee_cl_1", "galapagos_sea_lion",
-                     "south_american_fur_seal_cl_1", "hooded_seal", "mediterranean_monk_seal",
-                     "hawaiian_monk_seal", "bearded_seal_cl_1", "crabeater_seal",
-                     "leopard_seal", "arctic_ringed_seal", "ross_seal",
-                     "weddell_seal_cl_2", "northern_fur_seal_cl_1", "atlantic_walrus_cl_2",
-                     "nes_cl_2", "ses_cl_4", "california_sea_lion", "south_american_sea_lion",
-                     "new_zealand_sea_lion", "saimaa_ringed_seal_cl_1", "lagoda_ringed_seal",
-                     "baltic_ringed_seal", "new_zealand_fur_seal", "australian_fur_seal")]
+# seals <- all_seals[c("antarctic_fur_seal", "galapagos_fur_seal", "stellers_sea_lion_cl_1",
+#                      "grey_seal_orkneys", "harbour_seal_waddensee_cl_1", "galapagos_sea_lion",
+#                      "south_american_fur_seal_cl_1", "hooded_seal", "mediterranean_monk_seal",
+#                      "hawaiian_monk_seal", "bearded_seal_cl_1", "crabeater_seal",
+#                      "leopard_seal", "arctic_ringed_seal", "ross_seal",
+#                      "weddell_seal_cl_2", "northern_fur_seal_cl_1", "atlantic_walrus_cl_2",
+#                      "nes_cl_2", "ses_cl_4", "california_sea_lion", "south_american_sea_lion",
+#                      "new_zealand_sea_lion", "saimaa_ringed_seal_cl_1", "lagoda_ringed_seal",
+#                      "baltic_ringed_seal", "new_zealand_fur_seal", "australian_fur_seal")]
 
 
 # modify and simplify seal names
-names(seals) <- str_replace(names(seals), "_cl_1", "")
-names(seals) <- str_replace(names(seals), "_cl_2", "")
+# names(seals) <- str_replace(names(seals), "_cl_1", "")
+# names(seals) <- str_replace(names(seals), "_cl_2", "")
 
-# load abundances
-abundances <- read_excel("../data/abundances.xlsx", sheet = 1)
-
-# load generation times
-gen_time <- read_excel("../data/seal_data_krueger.xlsx", sheet = 1)[c("dataset_name", "Generation time")]
-names(gen_time) <- str_replace_all(names(gen_time), " ", "_")
-gen_time <- gen_time %>% 
-  filter(!is.na(dataset_name)) %>% 
-  # generation time of arctic ringed seal for other ringed seals
-  mutate(Generation_time = ifelse(is.na(Generation_time), 6804, Generation_time)) %>%
-  mutate(gen_time = Generation_time / 356)
-# mean(gen_time$gen_time)
+# # load abundances
+# abundances <- read_excel("../data/abundances.xlsx", sheet = 1)
+# 
+# # load generation times
+# gen_time <- read_excel("../data/seal_data_krueger.xlsx", sheet = 1)[c("dataset_name", "Generation time")]
+# names(gen_time) <- str_replace_all(names(gen_time), " ", "_")
+# gen_time <- gen_time %>% 
+#   filter(!is.na(dataset_name)) %>% 
+#   # generation time of arctic ringed seal for other ringed seals
+#   mutate(Generation_time = ifelse(is.na(Generation_time), 6804, Generation_time)) %>%
+#   mutate(gen_time = Generation_time / 356)
+# # mean(gen_time$gen_time)
 
 
 
 # which species? -----------------------------------------
 
-species_name <- "antarctic_fur_seal"
+# species_name <- "antarctic_fur_seal"
 
 # parameters for species
-N_pop <- as.numeric(abundances[abundances$species == species_name, "Abundance"])
+# N_pop <- as.numeric(abundances[abundances$species == species_name, "Abundance"])
 
-N_pop <- 10000
+# N_pop <- 100000
 
 # generation time in years
-gen_time <- as.numeric(gen_time[gen_time$dataset_name == species_name, "gen_time"])
-gen_time <- 13.07 # mean generation time across species
+# gen_time <- as.numeric(gen_time[gen_time$dataset_name == species_name, "gen_time"])
+
 # sampling 
-N_samp <- 150
-N_loc <- 100
-##
 
 
-run_sim <- function(niter, N_pop, N_samp, N_loc, model = c("bottleneck", "neutral", "decline", "expansion"), gen_time) {
+
+gen_time <- 13.07 # mean generation time across species
+
+
+run_sim <- function(niter, N_pop, model, gen_time) {
   
+  N_samp <- round(runif(1, min = 20, max = 100), 0)
+  N_loc <-  round(runif(1, min = 5, max = 30), 0)
+  # N_loc <- 20
   
-  if (niter%%1000 == 0) {
+  if (niter%%5000 == 0) {
     if (!file.exists("num_iter/iterations.txt")){
       iter_df <- data.frame(model, niter)
-      write.table(iter_df, file = "num_iter/iterations.txt", row.names = FALSE, col.names = FALSE)
+      write.table(iter_df, file = "num_iter/iterations.txt", row.names = FALSE, col.names = TRUE)
     } else {
       if (file.exists("num_iter/iterations.txt")){
-      all_iter <- read.table("num_iter/iterations.txt")
-      iter_df <- rbind(all_iter, c(model, niter))
-      write.table(iter_df, file = "num_iter/iterations.txt", row.names = FALSE, col.names = FALSE)
+      all_iter <- read.table("num_iter/iterations.txt", header = 1)
+      iter_df <- do.call(rbind, list(all_iter, data.frame(model, niter)))
+      write.table(iter_df, file = "num_iter/iterations.txt", row.names = FALSE, col.names = TRUE)
     }
     }
   }
@@ -95,8 +99,8 @@ run_sim <- function(niter, N_pop, N_samp, N_loc, model = c("bottleneck", "neutra
   Ne_prop <-   N_pop / N0  
   
   ## mutation rate
-  mu <- runif(1, min = 0.0005, max = 0.005)
-  # mu <- 0.0005
+  mu <- runif(1, min = 5e-05, max = 5e-03)
+  mu <- 0.00005
   ## theta
   theta <- 4 * N0 * mu
   
@@ -104,77 +108,102 @@ run_sim <- function(niter, N_pop, N_samp, N_loc, model = c("bottleneck", "neutra
   #### bottleneck end ####
   while (sequence_correct == FALSE) {
     # time is always thought in GENERATIONS
-    latest_end <- (2016 - 2000) / gen_time # generations ago, 2000 as latest end
-    earliest_end <- (2016 - 1800) / gen_time # generations ago, 1800 as earliest end
+    latest_end_bot <- (2016 - 1950) / gen_time # generations ago, 2000 as latest end
+    earliest_end_bot <- (2016 - 1800) / gen_time # generations ago, 1800 as earliest end
     # see ms manual. time is expressed in units of 4*N0
-    end_bot <- runif(1, min = latest_end / (4*N0), max = earliest_end / (4*N0))
+    end_bot <- runif(1, min = latest_end_bot / (4*N0), max = earliest_end_bot / (4*N0))
     
     #### bottleneck start ####
-    latest_start <- (2016 - 1900) / gen_time # generations ago, latest bottleneck start is 1900
-    earliest_start <- (2016 - 1600) / gen_time # generations ago, earliest bottleneck start is 1600
+    latest_start_bot <- (2016 - 1850) / gen_time # generations ago, latest bottleneck start is 1850
+    earliest_start_bot <- (2016 - 1700) / gen_time # generations ago, earliest bottleneck start is 1700
     # see ms manual. time is expressed in units of 4*N0
-    start_bot <- runif(1, min = latest_start / (4*N0), max = earliest_start / (4*N0))
+    start_bot <- runif(1, min = latest_start_bot / (4*N0), max = earliest_start_bot / (4*N0))
     
     # ensure that the end of the bottleneck is later than the start of the bottleneck
     if (end_bot < start_bot) sequence_correct <- TRUE
   }
   
-  ## bottleneck population size reduction from 1/1000 to 1/1000000
-  max_bot <- 100 / N0 # 100 individuals
+  ## bottleneck population size reduction from 1/1000 to 2 individuals
+  max_bot <- 1000 / N0 # 100 individuals
   min_bot <- 2 / N0   # 2 individuals
   N_bot <- round(runif(1, min = min_bot, max = max_bot), 7) 
   
+  # hist(rlnorm(1000, meanlog = 4, sdlog = 3), breaks = 1000)
+  # rtruncnorm(10000, a=0, b=1000, mean = 50, sd = 300)
+  
+  # N_bot <- rnorm(1, mean = 0.01, sd = 0.05)
+  
   ## historical population size ranges in the same absolute priors (range) as the current population size
-  N_hist <- round(runif(1, min = (1 / prop_prior_N) * Ne_prop, max = Ne_prop), 7)
-  #
+  N_hist_bot <- round(runif(1, min = (1 / prop_prior_N) * Ne_prop, max = Ne_prop), 7)
   
   # Ice age ended roughly 12000 years ago
-  earliest_start_decl <- 12000/gen_time 
-  # latest start of decline roughly 50 years ago
-  latest_start_decl <- 50/gen_time 
+  # earliest_start_decl_IA <- 12000/gen_time
+  # latest start of decline roughly 8000 years ago
+  # latest_start_decl_IA <- 8000/gen_time 
+  
+  # Truncated normal distribution including Ice Age End
+  start_decl_IA <- (truncnorm::rtruncnorm(n = 1, a=5000, b=19000, mean = 12000, sd = 1500) / gen_time) / (4*N0)
+  start_exp_IA <- (truncnorm::rtruncnorm(n = 1, a=5000, b=19000, mean = 12000, sd = 1500) / gen_time) / (4*N0)
   
   ## start of expansion and decline have same broad priors
+ # start_decl_IA <- runif(1, min =  latest_start_decl_IA / (4*N0), max =  earliest_start_decl_IA / (4*N0))
+ #  start_exp_IA <- runif(1, min =  latest_start_decl_IA / (4*N0), max =  earliest_start_decl_IA / (4*N0))
   
-  # start_decl <- runif(1, min =  latest_start_decl / (4*N0), max = earliest_start_decl / (4*N0))
-  # start_exp  <- runif(1, min =  latest_start_decl / (4*N0), max = earliest_start_decl / (4*N0))
+  ## recent decline and expansion similar to bottleneck
+  start_decl_recent <- runif(1, min = latest_start_bot / (4*N0), max = earliest_start_bot / (4*N0))
+  start_exp_recent <- runif(1, min = latest_start_bot / (4*N0), max = earliest_start_bot / (4*N0))
   
   # starts are now like the bottleneck start
-  start_decl <- runif(1, min = latest_start / (4*N0), max = earliest_start / (4*N0))
-  start_exp <- runif(1, min = latest_start / (4*N0), max = earliest_start / (4*N0))
+  # start_decl <- runif(1, min = latest_start / (4*N0), max = earliest_start / (4*N0))
+  # start_exp <- runif(1, min = latest_start / (4*N0), max = earliest_start / (4*N0))
+  
   # historical pop sizes
-  pop_param_decl_exp <- 100 # population was a maximum of 100 times larger or smaller in the past
+  pop_param_decl_exp <- 10 # population was a maximum of 10 times larger or smaller in the past
   
-  N_hist_decl <- round(runif(1, min = 1, max = pop_param_decl_exp), 7)
-  N_hist_exp <- round(runif(1, min = 1/pop_param_decl_exp, max = 1), 7)
+  # N_hist_exp <- round(runif(1, min = 1/pop_param_decl_exp, max = 1), 7)
+  # N_hist_decl <- round(runif(1, min = 1, max = pop_param_decl_exp), 7)
   
+  # truncated normal distribution
+  N_hist_decl <- truncnorm::rtruncnorm(n = 1,a = 1, b = 100, mean = pop_param_decl_exp, sd = 5)
+  N_hist_exp <- truncnorm::rtruncnorm(n = 1,a = 0, b = 1, mean = 1/pop_param_decl_exp, sd = 0.05)
   
+  # N_hist_exp <- rnorm(n = 1, mean = 1 / pop_param_decl_exp, sd = 0.05)
   
   # exponential population decline or increase
   # care: time is now start of bottleneck
-  alpha_decl <-  -(1/ start_decl) * log((N_hist_decl * N0) / N0)
-  alpha_exp <-   -(1/ start_exp) * log((N_hist_exp * N0) / N0 )
+  alpha_decl_IA <-  -(1/ start_decl_IA) * log((N_hist_decl * N0) / N0)
+  alpha_exp_IA <-   -(1/ start_exp_IA) * log((N_hist_exp * N0) / N0 )
   
+  alpha_decl_recent <-  -(1/  start_decl_recent) * log((N_hist_decl * N0) / N0)
+  alpha_exp_recent <-   -(1/ start_exp_recent) * log((N_hist_exp * N0) / N0 )
   # 
   
   if (model == "bottleneck") {
-    ms_options <- paste("-eN", end_bot, N_bot, "-eN", start_bot, N_hist, sep = " ")
+    ms_options <- paste("-eN", end_bot, N_bot, "-eN", start_bot, N_hist_bot, sep = " ")
   }
   
   if (model == "neutral") {
-    ms_options <- paste("-eN", start_bot, N_hist, sep = " ")
+    ms_options <- paste("-eN", start_bot, N_hist_bot, sep = " ")
   }
   
-  if (model == "decline") {
-    ms_options <- paste("-G", alpha_decl, "-eN", start_decl, N_hist_decl, sep = " ") # start_decl
+  if (model == "decline_recent") {
+    ms_options <- paste("-G", alpha_decl_recent, "-eN", start_decl_recent, N_hist_decl, sep = " ") 
   }
   
-  if (model == "expansion") {
-    ms_options <- paste("-G", alpha_exp, "-eN", start_exp, N_hist_exp, sep = " ") # start_exp
+  if (model == "expansion_recent") {
+    ms_options <- paste("-G", alpha_exp_recent, "-eN", start_exp_recent, N_hist_exp, sep = " ") 
   }
   
+  if (model == "decline_IA") {
+    ms_options <- paste("-G", alpha_decl_IA, "-eN", start_decl_IA, N_hist_decl, sep = " ") 
+  }
   
-  p_single = runif(1, min = 0.6, max = 0.95) # probability of multi-step mutation is 0.2
-  sigma2_g = runif(1, min = 5, max = 70) # typical step-size ~7
+  if (model == "expansion_IA") {
+    ms_options <- paste("-G", alpha_exp_IA, "-eN", start_exp_IA, N_hist_exp, sep = " ") 
+  }
+  
+  p_single = runif(1, min = 0.7, max = 0.99) # probability of multi-step mutation is 0.2
+  sigma2_g = runif(1, min = 5, max = 30) # typical step-size ~7
   
   simd_data <- as.data.frame(microsimr::sim_microsats(theta = theta,
                                                       n_ind = N_samp,
@@ -188,10 +217,12 @@ run_sim <- function(niter, N_pop, N_samp, N_loc, model = c("bottleneck", "neutra
   
   sim_param <- data.frame(N_pop, N0, mu, theta,
                           start_bot, end_bot,
-                          N_bot, N_hist,
-                          start_decl, start_exp,
+                          N_bot, N_hist_bot,
+                          start_decl_IA, start_decl_recent,
+                          start_exp_IA, start_exp_recent,
                           N_hist_decl, N_hist_exp,
-                          p_single, sigma2_g)
+                          p_single, sigma2_g,
+                          N_loc, N_samp)
   out <- cbind(sim_param, sum_stats)
   
   out
@@ -199,33 +230,46 @@ run_sim <- function(niter, N_pop, N_samp, N_loc, model = c("bottleneck", "neutra
 
 
 ### number of all simulations
-num_sim <- 10000
+num_sim <- 50000
 
 ######### all simulations for 100000 ###############
 # census size
-N_pop <- 10000
+N_pop <- 100000
 
-cl <- makeCluster(getOption("cl.cores", detectCores() - 1))
-clusterEvalQ(cl, c(library("strataG"), library("splitstackshape")))
 
-sims <- parLapply(cl, 1:num_sim, run_sim,  N_pop = N_pop, N_samp = N_samp, N_loc = N_loc, model = "bottleneck", gen_time = gen_time)
-sims_bot <- as.data.frame(data.table::rbindlist(sims))
+all_models = c("bottleneck", "neutral", "decline_recent", "expansion_recent", "decline_IA", "expansion_IA")
 
-sims <- parLapply(cl, 1:num_sim, run_sim,  N_pop = N_pop, N_samp = N_samp, N_loc = N_loc, model ="neutral", gen_time = gen_time)
-sims_neut <- as.data.frame(data.table::rbindlist(sims))
+run_sim_per_mod <- function(model){
+  
+  cl <- makeCluster(getOption("cl.cores", detectCores() - 2))
+  clusterEvalQ(cl, c(library("strataG"), library("splitstackshape")))
+  
+  sims <- parLapply(cl, 1:num_sim, run_sim,  N_pop = N_pop, model = model, gen_time = gen_time)
+  sims_df <- as.data.frame(data.table::rbindlist(sims))
+  
+  stopCluster(cl)
+  
+  sims_df
+  
+}
+# 
+# run_sim_per_mod <- function(model){
+#   sims <- lapply(1:num_sim, run_sim,  N_pop = N_pop, model = model, gen_time = gen_time)
+#   sims_df <- as.data.frame(data.table::rbindlist(sims))
+#   sims_df
+# }
 
-sims <- parLapply(cl, 1:num_sim, run_sim,  N_pop = N_pop, N_samp = N_samp, N_loc = N_loc, model ="decline", gen_time = gen_time)
-sims_decl <- as.data.frame(data.table::rbindlist(sims))
 
-sims <- parLapply(cl, 1:num_sim, run_sim,  N_pop = N_pop, N_samp = N_samp, N_loc = N_loc, model ="expansion", gen_time = gen_time)
-sims_exp <- as.data.frame(data.table::rbindlist(sims))
+sims <- do.call(rbind, lapply(all_models, run_sim_per_mod))
+sims$model <- c(rep("bot", num_sim), rep("neut", num_sim), rep("decl_IA", num_sim), 
+                rep("exp_IA", num_sim), rep("decl_rec", num_sim), rep("exp_rec", num_sim))
 
-stopCluster(cl)
+write.table(sims, file = "sims_50000_6mod_lowmu.txt", row.names = FALSE)
 
-sims <- rbind(sims_bot, sims_neut, sims_decl, sims_exp) #sims_exp
-sims$model <- c(rep("bot", num_sim), rep("neut", num_sim), rep("decl", num_sim), rep("exp", num_sim))
+# sims <- rbind(sims_bot, sims_neut, sims_decl_IA, sims_exp_IA, sims_decl_rec, sims_exp_rec) #sims_exp
 
-write.table(sims, file = "sims_10000.txt", row.names = FALSE)
+
+
 
 
 
